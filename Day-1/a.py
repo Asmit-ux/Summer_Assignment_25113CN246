5c3d72badfe53fc9ba44440e611a62ce36ed4763
Asmit - ux <asmit07oct@gmail.com>
2026-06-02 01:26:17 +0530

    Day-1 assignment is uploaded

---
+++ Day-1/a.py
@@ -0,0 +1,13 @@
+# METHOD - 1
+
+# a = int(input("Enter the N natural number - "))
+# print(a)
+# s = 0
+# for i in range(1,a+1):
+#     s += i
+# print(s)
+
+# METHOD - 2
+
+b = int(input('Enter a number - '))
+print(b * (b + 1) // 2)
