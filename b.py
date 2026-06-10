d03e67c002d187dbb3628f0664095d15a0b3efe9
Asmit K. Singh <asmit07oct@gmail.com>
2026-06-11 00:14:17 +0530

    Delete b.py

--- b.py
+++
@@ -1,8 +0,0 @@
-# Q.) Write a program to print number triangle
-
-n = 6
-
-for i in range(1, n+1):                   # using for loop to print the column
-    for j in range(1, i):                 # using for loop to print row
-        print(j,end =" ")
-    print()
